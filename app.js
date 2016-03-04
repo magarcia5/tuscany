5e49a270b6ea3c8b00c776e0d3796a041c3ddf5e
Mel <melaniegarcia90@gmail.com>
2016-03-04 12:23:16 -0800

    fixing random 404 error for favicon that randomly just started happening

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -36,7 +36,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(passport.initialize());
-app.use(jwt({ secret: 'SECRET', userProperty: 'payload'}).unless({path: ['/', '/login', '/register']}));
+app.use(jwt({ secret: 'SECRET', userProperty: 'payload'}).unless({path: ['/', '/login', '/register', '/favicon.ico']}));
 
 app.use('/', routes);
 app.use('/users', users);

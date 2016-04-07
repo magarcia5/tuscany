560fb3eada187a6fa9bf64b5d6d8d127503c82bc
Mel Alrajhi <mgarcia@splunk.com>
2016-04-07 15:14:03 -0700

    model modifications with individual field validatin

--- models/Trip.js
+++ models/Trip.js
@@ -2,28 +2,57 @@ var mongoose = require('mongoose');
 var TripLeg = mongoose.model('TripLeg').schema;
 
 var TripSchema = new mongoose.Schema({
-	name: String,
-	start_date: Date,
-	end_date: Date,
-	destination: mongoose.Schema.Types.Mixed,
-	transportation: String,
-	accomodation_addr: String,
+	name: { 
+		type: String, 
+		required: [ true, 'You must name the trip' ] 
+	},
+	start_date: { 
+		type: Date, 
+		required: [ true, 'Your trip needs a start date' ] 
+	},
+	end_date: { type: Date },
+	destination: { 
+		type: mongoose.Schema.Types.Mixed, 
+		required: [ true, 'You trip needs a destination' ],
+		validate: {
+			validator: function(v){
+				valid = v.formatted_address ? true : false;
+				return valid;
+			},
+			message: 'You need to pick a valid google destination.'
+		}
+	},
+	transportation: { 
+		type: String, 
+		enum: ['car', 'plane', 'train', 'walk', 'bike' ],
+		required: [ true, 'You need to pick a method of transportation. ']
+	},
+	accomodation_addr: {
+		type: mongoose.Schema.Types.Mixed,
+		validate: {
+			validator: function(v){
+				valid = v.formatted_address ? true : false;
+				return valid;
+			},
+			message: 'You need to pick a valid google accomodation address.'
+		}
+	},
 	// TODO uncomment when you narrow down google autocomplete and can determine what type of address got selected
 	//accomodation_type: String,
 	legs: [TripLeg]
 });
 
-TripSchema.methods.validateTrip = function(){
-
-	if(!this.destination.formatted_address){
-		return {valid: false, err: 'You need to pick a valid destination.'};
+TripSchema.methods.valid_dates = function(same_day){
+	if(!same_day && !this.end_date){
+		return false;
 	}
+
 	if(this.end_date < this.start_date){
-		return {valid: false, err: 'Your end date must be after or the same as the start date.'};
+		return false;
 	}
 
 	// TODO make sure the date isn't in the past
-	return {valid: true};
+	return true;
 };
 
 mongoose.model('Trip', TripSchema);
--- models/TripLeg.js
+++ models/TripLeg.js
@@ -1,33 +1,58 @@
 var mongoose = require('mongoose');
 
 var TripLegSchema = new mongoose.Schema({
-	name: String,
-	start_date: Date,
+	name: {
+		type: String,
+		required: [true, 'You need to name your trip leg']
+	},
+	start_date: { 
+		type: Date,
+		required: [true, 'You leg needs a start date']
+	},
 	end_date: Date,
-	destination: mongoose.Schema.Types.Mixed,
-	transportation: String,
-	accomodation_addr: String
+	destination: {
+		type: mongoose.Schema.Types.Mixed,
+		validate: {
+			validator: function(v){
+				valid = v.formatted_address ? true : false;
+				return valid;
+			},
+			message: 'You need to pick a valid google destination.'
+		}
+	},
+	transportation: {
+		type: String,
+		required: [true, 'You need to pick a method of transportation for your leg.'],
+		enum: ['walk', 'drive', 'fly', 'car', 'bike']
+	},
+	accomodation_addr: {
+		type: String,
+		validate: {
+			validator: function(v){
+				valid = v.formatted_address ? true : false;
+				return valid;
+			},
+			message: 'Your accomodations need to be a valid google address.'
+		}
+	}
 	// TODO uncomment when you narrow down google autocomplete and can determine what type of address got selected
 	//accomodation_type: String,
 });
 
-TripLegSchema.methods.validateLeg = function(trip_start_date, trip_end_date){
-	// console.log("Validating trip.");
-	// console.log(this);
-	// TODO make sure multiple leg trips dont overlap
-	if(!this.destination.formatted_address){
-		return {valid: false, err: 'You need to pick a valid destination for \"' + this.name + "\"."};
+TripLegSchema.methods.valid_dates = function(same_day, trip_start_date, trip_end_date){
+	if(!same_day && !trip.end_date){
+		return false;
 	}
 
 	if(this.end_date < this.start_date){
-		return {valid: false, err: 'Your end date must be after or the same as the start date for leg \"' + this.name + "\"."};
+		return false;
 	}
 
 	if(this.start_date < trip_start_date || this.start_date > trip_end_date
 		|| this.end_date < trip_start_date || this.end_date > trip_end_date){
-		return {valid: false, err: 'Leg \"' + this.name + '\" must be during the trip.'};
+		return false;
 	}
-	return {valid: true};
+	return true;
 };
 
 mongoose.model('TripLeg', TripLegSchema);
